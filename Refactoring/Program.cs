e5b9f83c3b65f9004463c33f91a7edcb67e6c524
Dave Mateer <davemateer@gmail.com>
2015-08-24 05:32:45 +0100

    first commit of reading a file of quotes

---
+++ Refactoring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Refactoring
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Read a file.. go towards ETL..Procedural..OO..Functional..funny quotes
+            var fileTextLines = File.ReadAllLines(@"..\..\quotes.csv");
+            foreach (var line in fileTextLines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
