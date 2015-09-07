16cb8acc3c7fba86ac72a25e311a96f822d665fa
Dave Mateer <davemateer@gmail.com>
2015-09-07 05:47:28 +0100

    Testing ReadFile list and passing in the mock log

--- QuoteImporterFunctional/ImporterFunctional.cs
+++ QuoteImporterFunctional/ImporterFunctional.cs
@@ -4,8 +4,6 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuoteImporterFunctional
 {
--- QuoteImporterFunctional/ImporterFunctionalTests.cs
+++ QuoteImporterFunctional/ImporterFunctionalTests.cs
@@ -1,141 +1,146 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Xunit;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
-//namespace QuoteImporterFunctional
-//{
-//    public class ImporterFunctionalTests
-//    {
-//        // 1. Testing the static functions which have no dependecies
-//        [Fact]
-//        public void CreateReport_GivenDave_ShouldReturnDaveDone()
-//        {
-//            var result = ImporterFunctional3Spike.ReadFileListOfLines();
-//            Assert.Equal(3, result.Count());
-//        }
+namespace QuoteImporterFunctional
+{
+    public class ImporterFunctionalTests
+    {
+        // 1. Static functions
+        [Fact]
+        public void ReadFileList_x_ShouldReturnListOfLines()
+        {
+            // mocking out log
+            // Action is a delegate which doesn't return a value
+            // Passing in an lambda (anonymous function) which does nothing
+            Action<string> log = s => { };
 
-//        [Fact]
-//        public void ParseLine_AValidLine_ShouldReturnQuote()
-//        {
-//            string line = "Basic,Programming in Basic causes brain damage - Edsger Wybe Dijkstra";
+            IEnumerable<string> result = ImporterFunctional.ReadFileListOfLines(log);
+            Assert.Equal(3, result.Count());
+        }
 
-//            Quote result = ImporterFunctional3Spike.ParseLine(line);
+        [Fact]
+        public void ParseLine_AValidLine_ShouldReturnQuote()
+        {
+            string line = "Basic,Programming in Basic causes brain damage - Edsger Wybe Dijkstra";
 
-//            Assert.Equal("Basic", result.Title);
-//            Assert.Equal("Programming in Basic causes brain damage - Edsger Wybe Dijkstra", result.Body);
-//        }
+            Quote result = ImporterFunctional.ParseLine(line);
 
-//        [Fact]
-//        public void ParseLine_AnEmptyLine_ShouldThrow()
-//        {
-//            string line = "";
+            Assert.Equal("Basic", result.Title);
+            Assert.Equal("Programming in Basic causes brain damage - Edsger Wybe Dijkstra", result.Body);
+        }
 
-//            Assert.Throws<ApplicationException>(() => ImporterFunctional3Spike.ParseLine(line));
-//        }
+        [Fact]
+        public void ParseLine_AnEmptyLine_ShouldThrow()
+        {
+            string line = "";
 
-//        [Fact]
-//        public void ParseLine_TooManyCommas_ShouldThrow()
-//        {
-//            string line = "asdf,asdf,asdf";
+            Assert.Throws<ApplicationException>(() => ImporterFunctional.ParseLine(line));
+        }
 
-//            Assert.Throws<ApplicationException>(() => ImporterFunctional3Spike.ParseLine(line));
-//        }
+        [Fact]
+        public void ParseLine_TooManyCommas_ShouldThrow()
+        {
+            string line = "asdf,asdf,asdf";
 
-//        // 2. Testing the main function - GetProcessing which has 4 dependencies 
-//        // mocking out the dependencies, and testing what this main function
-//        // does to it's dependencies
-//        [Fact]
-//        public void RunProcessing_GivenQuoteMock_ShouldInsertIntoMockDatabase()
-//        {
-//            // mocking out QuoteImporter's 4 dependencies
-//            // Action is a delegate which doesn't return a value
-//            // Passing in an anonymous function which does nothing
-//            Action<string> log = x => { };
+            Assert.Throws<ApplicationException>(() => ImporterFunctional.ParseLine(line));
+        }
 
-//            // mock file reader
-//            string actualLineFromFile = "";
-//            Func<IEnumerable<string>> readFileListOfLines = delegate
-//            {
-//                var listOfLines = new[] { "title, quote here" };
-//                actualLineFromFile = listOfLines[0];
-//                return listOfLines;
-//            };
+        // 2. Testing the main function - GetProcessing which has 4 dependencies 
+        // mocking out the dependencies, and testing what this main function
+        // does to it's dependencies
+        [Fact]
+        public void RunProcessing_GivenQuoteMock_ShouldInsertIntoMockDatabase()
+        {
+            // mocking out QuoteImporter's 4 dependencies
+            // Action is a delegate which doesn't return a value
+            // Passing in an anonymous function which does nothing
+            Action<string> log = x => { };
 
-//            // mock the parser
-//            string actualLineSentToParser = "";
-//            Func<string, Quote> parseLine = delegate (string s)
-//            {
-//                actualLineSentToParser = s;
-//                var quote = new Quote { Title = "title2", Body = "quote here2" };
-//                return quote;
-//            };
+            // mock file reader
+            string actualLineFromFile = "";
+            Func<IEnumerable<string>> readFileListOfLines = delegate
+            {
+                var listOfLines = new[] { "title, quote here" };
+                actualLineFromFile = listOfLines[0];
+                return listOfLines;
+            };
 
-//            // Mock the database insert
-//            // using a closure
-//            IList<Quote> actualQuotes = new List<Quote>();
-//            Action<Quote> insertQuoteIntoDatabase = quote =>
-//            {
-//                actualQuotes.Add(quote);
-//            };
+            // mock the parser
+            string actualLineSentToParser = "";
+            Func<string, Quote> parseLine = delegate (string s)
+            {
+                actualLineSentToParser = s;
+                var quote = new Quote { Title = "title2", Body = "quote here2" };
+                return quote;
+            };
 
-//            // compose.  Action is a delegate - doesn't return anything
-//            Action run = () => ImporterFunctional3Spike.QuoteImporter(log,
-//                readFileListOfLines, parseLine, insertQuoteIntoDatabase);
-//            run();
+            // Mock the database insert
+            // using a closure
+            IList<Quote> actualQuotes = new List<Quote>();
+            Action<Quote> insertQuoteIntoDatabase = quote =>
+            {
+                actualQuotes.Add(quote);
+            };
 
-//            // Testing the mock reader is in the state we expect
-//            Assert.Equal("title, quote here", actualLineFromFile);
+            // compose.  Action is a delegate - doesn't return anything
+            Action run = () => ImporterFunctional.QuoteImporter(log,
+                readFileListOfLines, parseLine, insertQuoteIntoDatabase);
+            run();
 
-//            // Testing the mock parser is in the state we expect
-//            Assert.Equal("title, quote here", actualLineSentToParser);
+            // Testing the mock reader is in the state we expect
+            Assert.Equal("title, quote here", actualLineFromFile);
 
-//            // Testing the mock database is in the state we expect
-//            Assert.Equal("title2", actualQuotes[0].Title);
-//            Assert.Equal("quote here2", actualQuotes[0].Body);
-//        }
+            // Testing the mock parser is in the state we expect
+            Assert.Equal("title, quote here", actualLineSentToParser);
 
-//        //[Fact]
-//        //public void RunProcessingShouldLog()
-//        //{
-//        //    var listOfLogEntries = new List<string>();
-//        //    // Action is a delegate - doesn't return.  
-//        //    Action<string> log = m => listOfLogEntries.Add(m);
+            // Testing the mock database is in the state we expect
+            Assert.Equal("title2", actualQuotes[0].Title);
+            Assert.Equal("quote here2", actualQuotes[0].Body);
+        }
 
-//        //    // Func input of a string, returns string.. 
-//        //    Func<string, string> createReport = x => x + " report test";
+        //[Fact]
+        //public void RunProcessingShouldLog()
+        //{
+        //    var listOfLogEntries = new List<string>();
+        //    // Action is a delegate - doesn't return.  
+        //    Action<string> log = m => listOfLogEntries.Add(m);
 
-//        //    // Func with no input, delegating to an anonymous function, 
-//        //    // returning an array of 1 string
-//        //    Func<IEnumerable<string>> getCustomers = () => new[] { "ellie" };
+        //    // Func input of a string, returns string.. 
+        //    Func<string, string> createReport = x => x + " report test";
 
-//        //    Action<string, string> sendEmail = (toAddress, body) => { };
+        //    // Func with no input, delegating to an anonymous function, 
+        //    // returning an array of 1 string
+        //    Func<IEnumerable<string>> getCustomers = () => new[] { "ellie" };
 
-//        //    ImporterFunctional3Spike.QuoteImporter(log, getCustomers, createReport, sendEmail);
+        //    Action<string, string> sendEmail = (toAddress, body) => { };
 
-//        //    // assert logging in working
-//        //    Assert.Equal(3, listOfLogEntries.Count);
-//        //    Assert.Equal("test", listOfLogEntries[0]);
-//        //    Assert.Equal("ellie", listOfLogEntries[1]);
-//        //    Assert.Equal("ellie report test", listOfLogEntries[2]);
-//        //}
+        //    ImporterFunctional3Spike.QuoteImporter(log, getCustomers, createReport, sendEmail);
 
-//        //[Fact]
-//        //public void RunProcessingShouldSendMultipleCorrectEmailBodies()
-//        //{
-//        //    Action<string> log = m => { };
-//        //    Func<string, string> createReport = customer => customer + " done";
-//        //    Func<IEnumerable<string>> getCustomers = () => new[] { "ellie", "bob" };
+        //    // assert logging in working
+        //    Assert.Equal(3, listOfLogEntries.Count);
+        //    Assert.Equal("test", listOfLogEntries[0]);
+        //    Assert.Equal("ellie", listOfLogEntries[1]);
+        //    Assert.Equal("ellie report test", listOfLogEntries[2]);
+        //}
 
-//        //    // closure
-//        //    var actualBodyList = new List<string>();
-//        //    Action<string, string> sendEmail = (toAddress, body) => actualBodyList.Add(body);
+        //[Fact]
+        //public void RunProcessingShouldSendMultipleCorrectEmailBodies()
+        //{
+        //    Action<string> log = m => { };
+        //    Func<string, string> createReport = customer => customer + " done";
+        //    Func<IEnumerable<string>> getCustomers = () => new[] { "ellie", "bob" };
 
-//        //    ImporterFunctional3Spike.QuoteImporter(log, getCustomers, createReport, sendEmail);
+        //    // closure
+        //    var actualBodyList = new List<string>();
+        //    Action<string, string> sendEmail = (toAddress, body) => actualBodyList.Add(body);
 
-//        //    Assert.Equal("ellie done", actualBodyList[0]);
-//        //    Assert.Equal("bob done", actualBodyList[1]);
-//        //}
-//    }
-//}
+        //    ImporterFunctional3Spike.QuoteImporter(log, getCustomers, createReport, sendEmail);
+
+        //    Assert.Equal("ellie done", actualBodyList[0]);
+        //    Assert.Equal("bob done", actualBodyList[1]);
+        //}
+    }
+}
