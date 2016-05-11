ba81e595289b6b130842a985829d9468cf6624f1
Dave Mateer <davemateer@gmail.com>
2016-05-11 07:14:11 +0100

    added in LSCC

---
+++ RefactoringLSCC/PalindromeTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringLSCC
+{
+    public class PalindromeTester
+    {
+
+        public bool Test(string strInput)
+        {
+            string strTrimmed = strInput.Replace(" ", "");
+            string strReversed = new string(strTrimmed.Reverse().ToArray());
+            return strReversed.Equals(strReversed);
+        }
+
+        public bool Check(string input)
+        {
+            input = input.Replace(" ", "");
+            var reversed = new string(input.Reverse().ToArray());
+            return reversed.Equals(input);
+        }
+
+        public bool IsPalindrome(string input)
+        {
+            var forwards = input.Replace(" ", "");
+            var backwards = new string(forwards.Reverse().ToArray());
+            return backwards.Equals(forwards);
+        }
+    }
+}
