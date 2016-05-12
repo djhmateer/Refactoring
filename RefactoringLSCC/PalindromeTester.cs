1cbcc65b60d3f1d9182a4b21c023986030cb32f4
Dave Mateer <davemateer@gmail.com>
2016-05-12 07:05:28 +0100

    palindromes

--- RefactoringLSCC/PalindromeTester.cs
+++ RefactoringLSCC/PalindromeTester.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using Xunit;
 
 namespace RefactoringLSCC
 {
     public class PalindromeTester
     {
-
         public bool Test(string strInput)
         {
             string strTrimmed = strInput.Replace(" ", "");
@@ -28,6 +24,45 @@ namespace RefactoringLSCC
             var forwards = input.Replace(" ", "");
             var backwards = new string(forwards.Reverse().ToArray());
             return backwards.Equals(forwards);
+        }
+    }
+
+    public class Tests
+    {
+        [Fact]
+        public void Not_a_palindrome()
+        {
+            var p = new PalindromeTester();
+            var result = p.Check("hello");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void A_palindrome()
+        {
+            var p = new PalindromeTester();
+            var result = p.Check("noon");
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("hello world", false)]
+        [InlineData("noon", true)]
+        [InlineData("eva can i stab bats in a cave", true)]
+        //Mr.Owl ate my metal worm
+        //    Was it a car or a cat I saw?
+        //    A nut for a jar of tuna
+        //    Do geese see God?
+        //    On a clover, if alive erupts a vast pure evil, a fire volcano
+        //    Dammit, I'm mad!
+        //    A Toyota's a Toyota
+        //    Go hang a salami, I'm a lasagna hog
+        //    A Santa lived as a devil at NASA
+        public void Palindrome_sentences(string input, bool expected)
+        {
+            var p = new PalindromeTester();
+            var result = p.IsPalindrome(input);
+            Assert.Equal(expected, result);
         }
     }
 }
