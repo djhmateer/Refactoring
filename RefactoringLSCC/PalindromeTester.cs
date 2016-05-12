c822ae5d8ba6b8ccead78b8303d5047b4b9e3f48
Dave Mateer <davemateer@gmail.com>
2016-05-12 14:41:20 +0100

    ppt

--- RefactoringLSCC/PalindromeTester.cs
+++ RefactoringLSCC/PalindromeTester.cs
@@ -48,16 +48,8 @@ namespace RefactoringLSCC
         [Theory]
         [InlineData("hello world", false)]
         [InlineData("noon", true)]
-        [InlineData("eva can i stab bats in a cave", true)]
-        //Mr.Owl ate my metal worm
-        //    Was it a car or a cat I saw?
-        //    A nut for a jar of tuna
-        //    Do geese see God?
-        //    On a clover, if alive erupts a vast pure evil, a fire volcano
-        //    Dammit, I'm mad!
-        //    A Toyota's a Toyota
-        //    Go hang a salami, I'm a lasagna hog
-        //    A Santa lived as a devil at NASA
+        [InlineData("mr owl ate my metal worm", true)]
+        [InlineData("do geese see god", true)]
         public void Palindrome_sentences(string input, bool expected)
         {
             var p = new PalindromeTester();
